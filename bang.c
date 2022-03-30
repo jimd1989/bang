ea88f13c7a5567740551f6780798dba28e3b18c7
jimd1989 <jamesdalrymple@protonmail.com>
2022-03-30 11:56:17 +0900

    WIP

--- bang.c
+++ bang.c
@@ -5,12 +5,20 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+/* TODO: 
+ * getopts
+ * flag for passing velocity along with message
+ * change cutoff etc at runtime
+ * per channel cutoff
+ * measure pulse length
+ * */
+
 #define BITS 8          /* Bit depth of audio input                 */
 #define CHAN 2          /* Number of channels of audio input        */
 #define SIGNED 1        /* Read samples with signs                  */
 #define RATE 48000      /* Sampling rate of audio input             */
 #define SHIFT 16        /* Bit shift used to emphasize peak samples */
-#define RESOLUTION 250  /* Sampling windows per second              */
+#define RESOLUTION 2000 /* Sampling windows per second              */
 
 typedef struct sio_hdl SioHdl;
 typedef struct sio_par SioPar;
@@ -38,7 +46,8 @@ typedef struct Buffer {
 
 void makeAudio(SioHdl **s, SioPar *p) {
 
-/* Initialize connection to sndio with proper settings. */
+/* Initialize connection to sndio with proper settings. The hardware may reject 
+ * the suggested buffer size. `makeBuffer` will recover from this. */
 
  *s = sio_open(SIO_DEVANY, SIO_REC, 0);
  if (*s == NULL) { errx(1, "couldn't open sound."); }
@@ -46,6 +55,7 @@ void makeAudio(SioHdl **s, SioPar *p) {
  p->bits = BITS;
  p->sig = SIGNED;
  p->rchan = CHAN;
+ p->appbufsz = RATE / RESOLUTION;
  if (!sio_setpar(*s, p)) { errx(1, "couldn't apply sound settings"); }
  if (!sio_getpar(*s, p)) { errx(1, "couldn't get sound settings");   }
  if (p->rchan != CHAN)   { errx(1, "expected stereo input");         }
@@ -60,7 +70,7 @@ Buffer makeBuffer(char *cu, char *mtl, char *ml, char *mr, SioPar *p) {
   int cutoff = atoi(cu);
   if (cutoff < 0 || cutoff > 255) { errx(1, "<cutoff> must be [0,255]"); }
   b.cutoff = (uint8_t)cutoff;
-  b.countdown = (uint32_t)atoi(mtl);
+  b.countdown = (uint32_t)abs(atoi(mtl));
   if (*ml == '-') { b.msgL = NULL; } else { b.msgL = ml; }
   if (*mr == '-') { b.msgR = NULL; } else { b.msgR = mr; }
   makeAudio(&b.audio, p);
@@ -80,8 +90,7 @@ void avg(Buffer *b, int n) {
 
 /* Add frame `n` (two interleaved stereo bytes) to the running average on each
  * channel. Samples are 8 bit, but absolute value renders them 7 bit. They are
- * cast to 32 bit for headroom during the averaging and peak emphasis process.
- * */
+ * cast to 32 bit for headroom during averaging and peak emphasis process. */
 
   uint32_t ln = (uint32_t)abs(b->data[n]);
   uint32_t rn = (uint32_t)abs(b->data[n + 1]);
@@ -95,11 +104,16 @@ void bang(Buffer *b) {
 
 /* If a channel is not currently muted and its average exceeds the cutoff, then
  * fire off its message to stdout. */
-  
-  warnx("BANG %u\n", b->avgL);
+
   b->countdownL--; b->countdownR--;
-  if (b->countdownL <= 0 && (b->avgL & 255) >= b->cutoff) {
-    printf("%s %d\n", b->msgL, b->avgL); fflush(stdout); b->countdownL = b->countdown;
+  /* Debug
+  printf("%d %d %d\n", b->avgL, b->countdownL, b->window);
+  */
+  if (b->msgL != NULL && b->countdownL <= 0 && (b->avgL & 255) >= b->cutoff) {
+    printf("%s %d\n", b->msgL); fflush(stdout); b->countdownL = b->countdown;
+  }
+  if (b->msgR != NULL && b->countdownR <= 0 && (b->avgR & 255) >= b->cutoff) {
+    printf("%s %d\n", b->msgR); fflush(stdout); b->countdownR = b->countdown;
   }
   b->tillBang = b->window;
 }
